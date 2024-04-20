6eeab27b38e8ed0024ff466d031cbc8843fb1d9f
Joseph Quatela <95594494+josephquatela@users.noreply.github.com>
2024-04-20 12:48:15 -0500

    4-20 work session

---
+++ MP-V3/ObjectAvoidance.py
@@ -0,0 +1,137 @@
+import time
+from Motor import *
+import RPi.GPIO as GPIO
+from servo import *
+from PCA9685 import PCA9685
+
+class ObjectAvoidance:
+    def __init__(self):
+        GPIO.setwarnings(False)
+        self.trigger_pin = 27
+        self.echo_pin = 22
+        self.MAX_DISTANCE = 300  # define the maximum measuring distance, unit: cm
+        self.timeOut = self.MAX_DISTANCE * 60  # calculate timeout according to the maximum measuring distance
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigger_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
+        self.angles = [23, 45, 68, 90, 113, 135, 158]
+        self.left_angles = [23, 45]
+        self.middle_angles = [68, 90, 113]
+        self.right_angles = [135, 158]
+        self.left_distances = []
+        self.middle_distances = []
+        self.right_distances = []
+        self.L = 0
+        self.M = 0
+        self.R = 0
+        self.PWM = Motor()
+        self.pwm = Servo()
+    
+    def resetDistanceArray(self):
+        self.left_distances.clear()
+        self.middle_distances.clear()
+        self.right_distances.clear()
+        
+    def scanLtoR(self):
+        self.resetDistanceArray()
+        for angle in self.angles:
+            self.pwm.setServoPwm('0', angle)
+            time.sleep(0.01)
+            distance = self.get_distance()  # Get the value
+
+            if angle in self.left_angles:
+                self.left_distances.append(distance)
+            if angle in self.middle_angles:
+                self.middle_distances.append(distance)
+            if angle in self.right_angles:
+                self.right_distances.append(distance)   
+        self.L = min(self.left_distances)
+        self.M = min(self.middle_distances)
+        self.R = min(self.right_distances)
+
+    def scanRtoL(self):
+        self.resetDistanceArray()
+        for angle in reversed(self.angles):
+            self.pwm.setServoPwm('0', angle)
+            time.sleep(0.01)
+            distance = self.get_distance()  # Get the value
+
+            if angle in self.left_angles:
+                self.left_distances.append(distance)
+            if angle in self.middle_angles:
+                self.middle_distances.append(distance)
+            if angle in self.right_angles:
+                self.right_distances.append(distance)
+        self.L = min(self.left_distances)
+        self.M = min(self.middle_distances)
+        self.R = min(self.right_distances)
+
+    def pulseIn(self, pin, level, timeOut):  # obtain pulse time of a pin under timeOut
+        t0 = time.time()
+        while (GPIO.input(pin) != level):
+            if ((time.time() - t0) > timeOut * 0.000001):
+                return 0;
+        t0 = time.time()
+        while (GPIO.input(pin) == level):
+            if ((time.time() - t0) > timeOut * 0.000001):
+                return 0;
+        pulseTime = (time.time() - t0) * 1000000
+        return pulseTime
+
+    def get_distance(self):  # get the measurement results of ultrasonic module,with unit: cm
+        distance_cm = [0, 0, 0, 0, 0]
+        for i in range(5):
+            GPIO.output(self.trigger_pin, GPIO.HIGH)  # make trigger_pin output 10us HIGH level
+            time.sleep(0.00001)  # 10us
+            GPIO.output(self.trigger_pin, GPIO.LOW)  # make trigger_pin output LOW level
+            pingTime = self.pulseIn(self.echo_pin, GPIO.HIGH, self.timeOut)  # read plus time of echo_pin
+            distance_cm[i] = pingTime * 340.0 / 2.0 / 10000.0  # calculate distance with sound speed 340m/s
+        distance_cm = sorted(distance_cm)
+        return int(distance_cm[2])
+
+    def stop_motor(self):
+        self.PWM.setMotorModel(0, 0, 0, 0)
+
+    def run_motor(self, L, M, R):
+        # Too Close - Back UP
+        # if (L < 30 and M < 30 and R < 30) or M < 30:
+        #     print("back up")
+        #     print(f"left{L} middle{M} right{R}")
+        #     self.PWM.setMotorModel(-1500, -1500, -1500, -1500)
+        #     time.sleep(0.1)
+        #     if L < R:
+        #         self.PWM.setMotorModel(1500, 1500, -1500, -1500)
+        #     else:
+        #         self.PWM.setMotorModel(-1500, -1500, 1500, 1500)
+        # elif L < 30 and M < 30:
+        #     self.PWM.setMotorModel(2000, 2000, -2000, -2000)
+        # elif R < 30 and M < 30:
+        #     self.PWM.setMotorModel(-2000, -2000, 2000, 2000)
+        # elif L < 20:
+        #     self.PWM.setMotorModel(2000, 2000, -1000, -1000)
+        #     if L < 10:
+        #         self.PWM.setMotorModel(2000, 2000, -1500, -1500)
+        # elif R < 20:
+        #     self.PWM.setMotorModel(-1000, -1000, 2000, 2000)
+        #     if R < 10:
+        #         self.PWM.setMotorModel(-1500, -1500, 1500, 1500)
+        # else:
+        print("forward")
+        self.PWM.setMotorModel(500, 500, 500, 500)
+
+    def run(self):
+        while True:
+            self.scanLtoR()
+            self.run_motor(self.L, self.M, self.R)
+            self.scanRtoL()
+            self.run_motor(self.L, self.M, self.R)
+
+if __name__ == '__main__':
+    print('Program is starting ... ')
+    try:
+        objectAVD = ObjectAvoidance()
+        objectAVD.run()
+    except KeyboardInterrupt:
+        # Stop motors and reset servo
+        objectAVD.PWM.setMotorModel(0, 0, 0, 0)
+        objectAVD.pwm_S.setServoPwm('0', 90)
---
+++ MP-V3/ProjectThread.py
@@ -0,0 +1,95 @@
+from threading import Thread, Lock, Condition
+import time
+from Motor import *
+import RPi.GPIO as GPIO
+from servo import *
+from PCA9685 import PCA9685
+from ObjectAvoidance import *
+##### Include your files here
+from Turn_To_Angle import *
+
+# Global variables for shared data and synchronization
+mutex = Lock()  # Mutex to ensure thread-safe access to shared variables
+wait_sound = Condition(mutex)  # Condition variable for sound detection
+wait_OV = Condition(mutex)  # Condition variable for object avoidance
+
+sound_detected = False  # Shared variable to signal sound detection
+object_avoidance_running = True  # Shared variable to indicate whether object avoidance is running
+
+def echolocation_thread():
+    print('Echolocation thread is starting...')
+    ##### Initialization of object
+    global sound_detected, object_avoidance_running
+    while True:
+        # Listen for Sound
+        echolocation.listen()
+        # Update local sound condition
+        sound_detected = echolocation.get_sound_status()
+        if sound_detected:
+            with mutex:
+                while object_avoidance_running:
+                    wait_OV.wait()
+                PWM.setMotorModel(0,0,0,0) # Stop Motors from OV    
+                ##### Run your motor here
+                echolocation.turn_to_angle()
+                ##### Stop your motor here
+                sound_detected = False
+                wait_sound.notify_all()
+
+def object_avoidance_thread():
+    print('Object Avoidance thread is starting...')
+    global sound_detected, object_avoidance_running
+    while True:
+        # L to R Scan - Allow Faster response
+        with mutex:
+            while sound_detected:
+                wait_sound.wait() # Block
+            object_avoidance_running = True
+            ## Move Motor Here
+            objectAVD.scanLtoR()
+            objectAVD.run_motor(objectAVD.L, objectAVD.M, objectAVD.R)
+            ##################
+            object_avoidance_running = False
+            wait_OV.notify()
+
+        # R to L Scan - Allow Faster response
+        with mutex:
+            while sound_detected:
+                wait_sound.wait() # Block
+            object_avoidance_running = True
+            ## Move Motor Here
+            objectAVD.scanRtoL()
+            objectAVD.run_motor(objectAVD.L, objectAVD.M, objectAVD.R)
+            ##################
+            object_avoidance_running = False
+            wait_OV.notify()
+
+
+if __name__ == '__main__':
+    try:
+        print('Program is starting ...')
+        
+        ##### Echolocation Object
+        echolocation = Turn_To_Angle()
+        ##### Run your calibration Here
+        echolocation.calibrate_angles()
+        ##### Initialize your object here
+        echolocation.init_mic()
+        
+        ##### Avoidance Object
+        objectAVD = ObjectAvoidance()
+
+        echolocation_thread = Thread(target=echolocation_thread)
+        echolocation_thread.daemon = True
+        echolocation_thread.start()
+
+        object_avoidance_thread = Thread(target=object_avoidance_thread)
+        object_avoidance_thread.daemon = True
+        object_avoidance_thread.start()
+
+        # Main program continues here if needed
+        while True:
+            time.sleep(1)  # Example sleep to keep the main program running
+
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program  will be  executed.
+        PWM.setMotorModel(0,0,0,0)
