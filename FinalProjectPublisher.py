c5be742ad08529ae5d8cd2abc013b50bccc80ee7
CHU² <64764533+TruEOrigion@users.noreply.github.com>
2024-04-28 09:42:48 -0500

    Update FinalProjectPublisher.py

--- FinalProjectPublisher.py
+++ FinalProjectPublisher.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 from Command import COMMAND as cmd
 from Motor import *   
 
+# Defining ZMQ publisher
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5555")
@@ -12,6 +13,8 @@ GPIO.setwarnings(False)
 Buzzer_Pin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Buzzer_Pin,GPIO.OUT)
+
+# Buzzer object
 class Buzzer:
     def run(self,command):
         if command!="0":
@@ -22,32 +25,34 @@ class Buzzer:
 if __name__=='__main__':
     B=Buzzer()
     PWM=Motor()   
+    # Command line control for prey car
     while(True):
         try:
-            i = input()
-            if (i=="w"):
+            i = input() # Waiting for user input, press enter after input
+            if (i=="w"): # Foward
                 print("w pressed")
                 PWM.setMotorModel(1500, 1500, 1500, 1500)
-            elif (i=="a"):
+            elif (i=="a"): # Turn Left
                 print("a pressed")
                 PWM.setMotorModel(-1500, -1500, 1500, 1500)
-            elif (i=="d"):
+            elif (i=="d"): # Turn Right
                 print("d pressed")        
                 PWM.setMotorModel(1500, 1500, -1500, -1500) 
-            elif (i=="s"):
+            elif (i=="s"): # Stop
                 print("s pressed")        
                 PWM.setMotorModel(0,0,0,0)
-            elif(i=="b"):
-
-                message = "Sound Detected"
+            elif(i=="b"): # Buzzer
+                # Send ZMQ message to predator
+                message = "Sound Detected" 
                 socket.send_string(message)
                 print("Sent:", message)
-                time.sleep(2)
+                time.sleep(2) # Wait for predator to process
 
+                # Sound buzzer
                 B.run('1')
                 time.sleep(.5)
                 B.run('0')
-            elif(i=="q"):
+            elif(i=="q"): # Quit program
                 PWM.setMotorModel(0,0,0,0)
                 B.run('0')
                 break
